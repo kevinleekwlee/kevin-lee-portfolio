8d10fb3e159b63f932fdbedfd7505f0912b62454
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-12 17:51:14 -0400

    Building navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import { images } from '../../constants'
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div>
+        <img src={images.logo} alt="logo"/>
+      </div>
+      <ul>
+        {['home','about','work','skills','contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div/>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
