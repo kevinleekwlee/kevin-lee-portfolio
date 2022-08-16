622da6bb0862a8cc0ba617bb9c450298bcefc05f
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-16 14:22:39 -0400

    Building header jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -4,6 +4,17 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <div className="app__header app__flex">
@@ -44,9 +55,15 @@ const Header = () => {
       </motion.div>
 
       <motion.div
-        
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"        
       >
-
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle"/>
+          </div>
+        ))}
       </motion.div>
     </div>
   )
