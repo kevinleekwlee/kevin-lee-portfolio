86a8721bc84a4b5777108a3ac052bf719620a78b
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-16 12:46:52 -0400

    Building hero image

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -21,10 +21,31 @@ const Header = () => {
             </div>
           </div>
 
-          <div>
-            
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
           </div>
         </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg"/>
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+
+      </motion.div>
+
+      <motion.div>
+
       </motion.div>
     </div>
   )
