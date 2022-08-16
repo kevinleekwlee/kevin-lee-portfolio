a3cc69714dd7cc266ee6d4a08ee422ee8b87632b
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-16 10:41:49 -0400

    Building mobile friendly nav menu

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -32,11 +32,13 @@ const Navbar = () => {
           >
 
             <HiX onClick={() => setToggle(false)} />
-            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-              <li key={item}>
-                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-              </li>
-            ))}
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+              ))}
+            </ul>  
           </motion.div>
         )}
       </div>
