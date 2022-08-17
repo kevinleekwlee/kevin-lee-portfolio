a263cb5c8d229dcaf0402ab00250ddccf7f7ec22
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-17 19:15:15 -0400

    Styling About container

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -15,11 +15,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that
-        <span>Good Design</span>
-        <br />
-        means
-        <span>Good Business</span>
+        I know that <span>Good Dev</span> <br /> means <span>Good Business</span>
       </h2>
 
       <div className="app__profiles">
