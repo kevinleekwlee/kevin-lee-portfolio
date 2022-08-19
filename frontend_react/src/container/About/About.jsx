958eae2995977f131d6d624d77e81dc92c7244fd
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-19 11:23:58 -0400

    Added AppWrap to About section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
-import './About.scss';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
+import './About.scss';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -41,4 +41,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
