b935d6cbe1318118036603bcc24e7b9bb58f94ce
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-09-14 15:15:13 -0400

    Building navbar mobile version

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              {['home', 'about', 'web dev portfolio', 'business portfolio', 'skills', 'brands', 'contact'].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                 </li>
