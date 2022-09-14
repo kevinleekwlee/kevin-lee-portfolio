54067e434bb539c747bad4a670424f07480d3b9b
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-09-14 15:56:23 -0400

    Removing unnecessary images

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,12 +1,7 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import cpp from '../assets/cpp.png';
-import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import node from '../assets/node.png';
 import react from '../assets/react.png';
@@ -20,13 +15,8 @@ import favicon from '../assets/favicon.png';
 export default {
   email,
   mobile,
-  cpp,
-  css,
-  figma,
-  flutter,
   git,
   graphql,
-  html,
   javascript,
   node,
   react,
